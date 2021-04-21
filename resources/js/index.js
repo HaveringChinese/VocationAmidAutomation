e327e899a64d77dd9567dba4f3b3052081b196cc
Nate Conrad <nathanielrconrad@gmail.com>
2021-04-20 20:54:21 -0700

    implement timeline animations and viewport-agnostic design
    worked locally but didn't push changes to repo for almost a month. this is version 1 of vocationamidautomation.com

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,18 +1,37 @@
-document.querySelector(".btn").addEventListener("click", function (event) {
-    var welcomeSound = new Audio("resources/audios/tim-kahn-welcome-nick.mp3");
-    
-    welcomeSound.play();
-    
-    document.querySelector("body").classList.add("darken");
-    
-    setTimeout(function () {
-        window.open("signup.html")}, 1000);
-    
-    setTimeout(function () {
-        document.querySelector("body").classList.remove("darken");
-       
-    }, 2000);
-               
+//causes text boxes to appear as their location enters the viewport
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+    (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+var items = document.querySelectorAll("body div");
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }   
+  }
+}
+//finds where viewport is currrently
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
+document.querySelector("button").addEventListener("click", function (event) {
+  document.querySelector("body").classList.add("darken");
+
+  setTimeout(function () {
+    window.open("signup.html");
+  }, 1000);
+
+  setTimeout(function () {
+    document.querySelector("body").classList.remove("darken");
+  }, 2000);
 });
 
-//, "_blank"
